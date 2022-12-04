835a10c60fcbab1c9422b4d600ec07a98b6f79e0
LeslieCheung <lesliechueng1996@icloud.com>
2022-12-04 22:31:05 +0800

    add alias

--- jest.config.js
+++ jest.config.js
@@ -11,10 +11,13 @@ const customJestConfig = {
   moduleNameMapper: {
     // Handle module aliases (this will be automatically configured for you soon)
     '^@/components/(.*)$': '<rootDir>/components/$1',
-
     '^@/pages/(.*)$': '<rootDir>/pages/$1',
+    '^@/utils/(.*)$': '<rootDir>/utils/$1',
+    '^@/models/(.*)$': '<rootDir>/models/$1',
   },
   testEnvironment: 'jest-environment-jsdom',
+  collectCoverage: true,
+  coverageDirectory: 'coverage',
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
